282d29bf7eac18380b6887b47f1a0ff4d781e59a
Andres Posadas <andresposadas@Andress-MacBook-Pro.local>
2018-03-03 11:41:35 -0500

    Reorganized file structure to make it more comprehensible and easy to navigate

---
+++ app/components/Entry/Log-In/styles.js
@@ -0,0 +1,28 @@
+import { StyleSheet } from 'react-native';
+export default StyleSheet.create({
+	containerStyle: {
+		padding: 20,	
+	},
+	inputStyle: {
+		height: 40,
+		backgroundColor: '#82ccdd',
+		marginBottom: 20,
+		paddingHorizontal: 10,
+	},
+	titleStyle: {
+		marginBottom: 20,
+		justifyContent: 'center',
+		alignContent: 'center',
+		textAlign: 'center',
+	},
+	buttonTextStyle: {
+		textAlign: 'center',
+		color: '#FFFFFF',
+		fontWeight: '700',
+	},
+	buttonContainerStyle: {
+		paddingVertical: 10,
+		backgroundColor: '#2980b9',
+		marginBottom: 10,
+	}
+});
