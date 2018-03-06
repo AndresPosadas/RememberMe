0cde98c4a1e754ec8e934b0bdc22e78c9f5b6b14
Shannon Guyot <shannonguyot97@gmail.com>
2018-03-05 21:07:59 -0500

    initial commit

---
+++ app/config/constants.js
@@ -0,0 +1,6 @@
+export const FIREBASE_API_KEY = "AIzaSyBhpk92iLzneV_SroVfXy350J3XASFzSx4";
+export const FIREBASE_AUTH_DOMAIN = "remindme-fba28.firebaseapp.com";
+export const FIREBASE_DATABASE_URL = "https://remindme-fba28.firebaseio.com";
+export const FIREBASE_PROJECT_ID = "remindme-fba28";
+export const FIREBASE_STORAGE_BUCKET = "";
+export const FIREBASE_MESSAGING_SENDER_ID = "619005283422";
