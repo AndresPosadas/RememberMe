faa4165915733903d5d632afaec5575c70813d09
Shannon Guyot <shannonguyot97@gmail.com>
2018-03-14 16:18:22 -0400

    added files for better input form for reminders

--- app/modules/home/scenes/Timed/Timed.js
+++ app/modules/home/scenes/Timed/Timed.js
@@ -1,48 +1,135 @@
 import React from 'react';
-var { View, StyleSheet, Alert, ScrollView, Text, ListView, TextInput } = require('react-native');
+import { Text, Alert } from 'react-native';
 
-import { Button } from 'react-native-elements'
 import { Actions } from 'react-native-router-flux';
 import { connect } from 'react-redux';
-import firebase from "../../../../config/firebase";
+
+import { actions as stuff } from "../../index"
+const { login } = stuff;
 
 import styles from "./styles"
 
-import { actions as auth, theme } from "../../../auth/index"
+import Form from "../../components/Form"
+import AuthContainer from "../../components/AuthContainer"
+import firebase from '../../../../config/firebase';
 
-const { color } = theme;
 const database = firebase.database();
+const auth = firebase.auth();
+
+const fields = [
+	{
+		key: 'title',
+		label: "Title",
+		placeholder: "Title",
+		autoFocus: false,
+		secureTextEntry: false,
+		value: "",
+		type: "title"
+	},
+	{
+		key: 'description',
+		label: "Description",
+		placeholder: "Description",
+		autoFocus: false,
+		secureTextEntry: false,
+		value: "",
+		type: "description"
+	},
+	{
+		key: 'address',
+		label: "Address",
+		placeholder: "Address",
+		autoFocus: false,
+		secureTextEntry: false,
+		value: "",
+		type: "address"
+	},
+	{
+		key: 'date',
+		label: "Date",
+		placeholder: "Date",
+		autoFocus: false,
+		secureTextEntry: false,
+		value: "",
+		type: "date"
+	},
+	{
+		key: 'time',
+		label: "Time",
+		placeholder: "Time",
+		autoFocus: false,
+		secureTextEntry: false,
+		value: "",
+		type: "time"
+	}
+];
+
+const error = {
+	general: "",
+	title: "",
+	description: "",
+	address: "",
+	date: "",
+	time: ""
+}
 
 class Timed extends React.Component {
-    constructor() {
-		
-        super();
-        this.state = {}
+	constructor() {
+		super();
+		this.state = {
+			error: error
+		}
 
-    }
+		this.onSubmit = this.onSubmit.bind(this);
+		this.onSuccess = this.onSuccess.bind(this);
+		this.onError = this.onError.bind(this);
 
-    render() {
-		
+		this.user = firebase.auth().currentUser;
+	}
+
+	onSubmit(data) {
+		this.setState({ error: error }); //clear out error messages
+
+		database.ref('users/' + this.user.uid).child('reminders').push(data)
+			.then(() => {
+				this.onSuccess();
+			})
+			.catch((error) => {
+				this.onError(error);
+			});
+	}
+
+	// Return to home screen if reminder was created successfully
+	onSuccess() {
+		Actions.Main();
+	}
+
+	// Display errors
+	onError(error) {
+		let errObj = this.state.error;
+
+		if (error.hasOwnProperty("message")) {
+			errObj['general'] = error.message;
+		} else {
+			let keys = Object.keys(error);
+			keys.map((key, index) => {
+				errObj[key] = error[key];
+			})
+		}
+		this.setState({ error: errObj });
+	}
+
+	render() {
 		return (
-		
-			<View style={styles.container}>
-				<View style={styles.headerContainer}>
-					<Text style={styles.title}>Please fill out the following information.</Text>
-				</View>
-				<Text style={styles.title}>Name:</Text>
-				<TextInput placeholder='Enter the name here.' underlineColorAndroid="transparent" />
-				<Text style={styles.title}>Description:</Text>
-				<TextInput placeholder='Enter the description here.' underlineColorAndroid="transparent" />
-				<Text style={styles.title}>Address:</Text>
-				<TextInput placeholder='Enter the address here.' underlineColorAndroid="transparent" />
-				<Text style={styles.title}>Date:</Text>
-				<TextInput placeholder='Enter the date here.' underlineColorAndroid="transparent" />
-				<Text style={styles.title}>Time:</Text>
-				<TextInput placeholder='Enter the time here.' underlineColorAndroid="transparent" />
-			</View>
-		
-		);		
-    }
+			<AuthContainer>
+				<Form fields={fields}
+					showLabel={false}
+					onSubmit={this.onSubmit}
+					buttonTitle={"ADD REMINDER"}
+					error={this.state.error} />
+			</AuthContainer>
+		);
+	}
 }
 
 export default connect(null, null)(Timed);
